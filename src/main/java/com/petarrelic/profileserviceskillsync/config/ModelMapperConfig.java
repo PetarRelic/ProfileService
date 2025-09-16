db70ee82226b46c1b0d4e0796bd4641206428aff
Petar Relic <petar.relic@gmail.com>
2025-09-16 10:31:42 +0200

    Added config package and moved ModelMapperConfig

--- src/main/java/com/petarrelic/profileserviceskillsync/config/ModelMapperConfig.java
+++ src/main/java/com/petarrelic/profileserviceskillsync/config/ModelMapperConfig.java
@@ -1,4 +1,4 @@
-package com.petarrelic.profileserviceskillsync;
+package com.petarrelic.profileserviceskillsync.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
